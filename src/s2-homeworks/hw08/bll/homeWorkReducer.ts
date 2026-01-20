07d79a3ecf7f0907dc350134fbf2b0cb996b4709
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-20 12:39:24 +0500

    hw8 done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,16 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let stateCopy = [...state]
+            if (action.payload === 'up') stateCopy.sort((a: UserType, b: UserType) => a.name.localeCompare(b.name))
+            if (action.payload === 'down') stateCopy.sort((a: UserType, b: UserType) => a.name.localeCompare(b.name)).reverse()
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            // need to fix
+            return stateCopy
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+          return state.filter((user: UserType) => user.age >= action.payload)
         }
         default:
             return state
